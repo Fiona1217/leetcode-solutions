763b445083e83f8d9765ca89ba63011f170f9cf4
HJL <167211523+Fiona1217@users.noreply.github.com>
2025-04-12 15:15:26 -0400

    Update 1075-ProjectEmployeesI.sql

--- 1075-ProjectEmployeesI/1075-ProjectEmployeesI.sql
+++ 1075-ProjectEmployeesI/1075-ProjectEmployeesI.sql
@@ -1,1 +1,3 @@
-SELECT Project.project_id, ROUND(AVG(experience_years), 2) AS average_years FROM Project LEFT JOIN Employee ON Project.employee_id = Employee.employee_id GROUP BY project_id;
+SELECT Project.project_id, ROUND(AVG(experience_years), 2) AS average_years
+FROM Project LEFT JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY project_id;
